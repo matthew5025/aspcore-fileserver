18222486735d775c0925f5348e86f630cda0d42b
matthew5025 <matthew5025@users.noreply.github.com>
2020-07-17 17:51:36 +0800

    Server (#9)
    * Permit content-type header
    
    * Enabled filename and downlaod

--- FileServer/FileServer/FileCallbackResult.cs
+++ FileServer/FileServer/FileCallbackResult.cs
@@ -2,29 +2,75 @@
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Core;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Net.Http.Headers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Net.Http.Headers;
-
 namespace FileServer
 {
+    /// <summary>
+    /// Represents an <see cref="ActionResult"/> that when executed will
+    /// execute a callback to write the file content out as a stream.
+    /// </summary>
     public class FileCallbackResult : FileResult
     {
         private Func<Stream, ActionContext, Task> _callback;
 
+        /// <summary>
+        /// Creates a new <see cref="FileCallbackResult"/> instance.
+        /// </summary>
+        /// <param name="contentType">The Content-Type header of the response.</param>
+        /// <param name="callback">The stream with the file.</param>
+        public FileCallbackResult(string contentType, Func<Stream, ActionContext, Task> callback)
+            : this(MediaTypeHeaderValue.Parse(contentType), callback)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="FileCallbackResult"/> instance.
+        /// </summary>
+        /// <param name="contentType">The Content-Type header of the response.</param>
+        /// <param name="callback">The stream with the file.</param>
         public FileCallbackResult(MediaTypeHeaderValue contentType, Func<Stream, ActionContext, Task> callback)
             : base(contentType?.ToString())
         {
             if (callback == null)
+            {
                 throw new ArgumentNullException(nameof(callback));
-            _callback = callback;
+            }
+
+            Callback = callback;
         }
 
+        /// <summary>
+        /// Gets or sets the callback responsible for writing the file content to the output stream.
+        /// </summary>
+        public Func<Stream, ActionContext, Task> Callback
+        {
+            get
+            {
+                return _callback;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                _callback = value;
+            }
+        }
+
+        /// <inheritdoc />
         public override Task ExecuteResultAsync(ActionContext context)
         {
             if (context == null)
+            {
                 throw new ArgumentNullException(nameof(context));
+            }
+
             var executor = new FileCallbackResultExecutor(context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>());
             return executor.ExecuteAsync(context, this);
         }
@@ -38,7 +84,8 @@ namespace FileServer
 
             public Task ExecuteAsync(ActionContext context, FileCallbackResult result)
             {
-                return result._callback(context.HttpContext.Response.Body, context);
+                SetHeadersAndLog(context, result, null, false, null);
+                return result.Callback(context.HttpContext.Response.Body, context);
             }
         }
     }
